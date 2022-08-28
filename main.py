5e10c1d6e5609632f56811499a7436a9f778be83
trungtlam15 <trungtlam15@gmail.com>
2022-08-27 21:49:59 -0500

    Practice finding max with recursive

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 
+def find_max(array):
+    max = 0
+    for i in array:
+        if i > max:
+            max = i
+    return(max)
 
-# Press the green button in the gutter to run the script.
+def find_max_recursive(array,i):
+    if i == len(array) - 1:
+        return array[i]
+    current_value = array[i]
+    max_in_array = find_max_recursive(array,i+1)
+    max_array = max(current_value,max_in_array)
+    return max_array
+
+
+
+
 if __name__ == '__main__':
-    pass
+    array = [5,76,12,43,6]
+    # max = find_max(array)
+    # print(max)
+
+    # max = find_max_recursive(array,0)
+    # print(max)
+
+
+
+
+
+
 
 
