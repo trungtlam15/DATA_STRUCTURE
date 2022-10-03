80ea50d3b091a761ed5c994027182ff392c154a4
trungtlam15 <trungtlam15@gmail.com>
2022-10-02 21:56:57 -0500

    dynamic programming: fibonacci first approach

--- dynamic_programming/fibonacci.py
+++ dynamic_programming/fibonacci.py
@@ -4,9 +4,18 @@ def find_fibonacci(n):
         return n
     return find_fibonacci(n-1)+find_fibonacci(n-2)
 
+def find_fibonacci_ver2(n):
+    array = []
+    array.append(0)
+    array.append(1)
 
+    for i in range(2,n+1):
+        array.append(array[i-1] + array[i-2])
 
+    return array[n]
 
 if __name__ == '__main__':
-    result = find_fibonacci(8)
-    print(result)
+    # result = find_fibonacci(8)
+    # print(result)
+
+    print(find_fibonacci_ver2(6))
